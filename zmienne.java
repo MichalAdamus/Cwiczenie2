e67230c2348f4d1ecc4d28916004fa33cbc90536
Michał Adamus <mb.adamus@gmail.com>
2021-03-15 22:55:02 +0100

    #3 zadanie nr 3 program z numerem domu, mieszkania i konta

--- zmienne.java
+++ zmienne.java
@@ -27,6 +27,16 @@ class zmienne{
 		System.out.println("Czy pełnoletnia ? "+isAdult);
 		System.out.println("Średnia ocen "+average);
 				
+		/*********** Zadanie 3 - Numer domu; mieszkania i konto ***********/
+		System.out.println();
 		
+		int HouseNumber = 989;
+		byte AppartmentNumber = 89;
+		int AccountBalance = 1000000;
+		
+		System.out.println("Numer domu "+HouseNumber);
+		System.out.println("Numer Mieszkania "+AppartmentNumber);
+		System.out.println("Stan konta "+AccountBalance);
+				
 	}
 }
