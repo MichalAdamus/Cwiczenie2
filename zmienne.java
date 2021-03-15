c8c234eeacc236fe5e38d631276b098fa9b3e1f0
Michał Adamus <mb.adamus@gmail.com>
2021-03-15 22:32:13 +0100

    #3 przechowanie informacji o płci wieku

--- zmienne.java
+++ zmienne.java
@@ -1,10 +1,21 @@
-class Variable{
+class zmienne{
 	public static void main(String args[]){
 		
 		int BirthYear=2001;
 		char gender='K';
-		system.out.println(BirthYear);
-		system.out.println(gender);
+		
+		System.out.println(BirthYear);
+		System.out.println(gender);
+		
+		// "Witaj "+"Marian"
+		// "Wiek: "+"51"
+		// "Rok urodzenia: "BirthYear"
+		
+		System.out.println();
+		
+		
+		System.out.println("Rok urodzenia: "+BirthYear);
+		System.out.println("gender: "+gender);
 		
 	}
 }
