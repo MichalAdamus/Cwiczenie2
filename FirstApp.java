e29c81f1cdb2af822d76ffc7673cd22ff6e00903
Michał Adamus <mb.adamus@gmail.com>
2021-03-15 17:31:19 +0100

    pierwsza apka w Java wraz z zadaniami

---
+++ FirstApp.java
@@ -0,0 +1,51 @@
+class FirstApp{
+	public static void main(String args[]){
+System.out.println();
+System.out.println();
+
+// Przywitaj się ze Światem 
+System.out.println ("Hello World");
+System.out.println ("Ale dzisiaj wieje wiatr");
+
+System.out.println();
+System.out.println();
+
+// Wiersz z Panem Pomidorem
+System.out.println ("Pan Pomidor wlazł na tyczkę");
+System.out.println ("I przedrzeźnia ogrodniczkę.");
+System.out.println ("\"Jak pan może,");
+System.out.println ("Panie Pomidorze?!\"");
+
+// wiersz z biedronką 
+System.out.println("-----------------------");
+System.out.println("Do biedronki przyszedł żuk");
+System.out.println("W okieneczko puk-puk-puk");
+System.out.println();
+System.out.println();
+System.out.println("Panieneczka widzi żuka:");
+System.out.println("\"Czego Pan tu u mnie szuka?\"");
+System.out.println("-----------------------");
+
+// Ksiązka
+System.out.println("Ksiązka:\nNazwa: kompendium Programisty Java\nLiczba Stron: 510");
+System.out.println();
+System.out.println();
+// Tabliczka mnożenia
+/*
+1	2	3
+2	4	6
+3	6	9
+*/ 
+System.out.println("1\t2\t3");
+System.out.println("2\t4\t6");
+System.out.println("3\t6\t9");
+
+System.out.println();
+System.out.println();
+// Zadanie nr3
+System.out.println("*********************************");
+System.out.println("**\"To, że milczę, nie znaczy,\t**");
+System.out.println("**\"że nie mam nic do powiedzenia\"**");
+System.out.println("*********************************");
+	}
+}
